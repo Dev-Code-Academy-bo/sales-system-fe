b37d67c8c9cbf8f537e9c6f70cc6b268ffaa4c5b
Rodrigo <roddriqoxd@gmail.com>
2024-11-09 17:07:07 -0400

    create project structure

---
+++ src/app/public/components/sales/create-sale/create-sale.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-sale',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-sale.component.html',
+  styleUrl: './create-sale.component.scss'
+})
+export class CreateSaleComponent {
+
+}
