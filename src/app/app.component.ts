637e2a615163c78645f652fa95543f3180ae43e5
roger choquehuanca <znarfgeror@gmail.com>
2024-11-09 19:15:30 -0400

    module create product was created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./public/components/login/login.component";
+import { CreateProductComponent } from "./private/components/product/create-product/create-product.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, 
-    LoginComponent
-  ],
+    RouterOutlet,
+    LoginComponent,
+    CreateProductComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/private/components/product/create-product/create-product.component.ts
+++ src/app/private/components/product/create-product/create-product.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.scss'
 })
 export class CreateProductComponent {
+  myForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      productName: ['', Validators.required],
+      description: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+  
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log('Form submitted:', this.myForm.value);
+    }
+  }
 }
